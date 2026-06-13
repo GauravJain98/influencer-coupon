fd1a89462fac508a06dbaa35963b8b62de43c32e
Gaurav Jain <gauravcdj.98@gmail.com>
2026-06-13 17:08:01 +0530

    feat: manual migrate file

---
+++ server/cmd/migrate.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+
+	"github.com/GauravJain98/influencer-coupon/server/app"
+	"github.com/GauravJain98/influencer-coupon/server/config"
+	"github.com/GauravJain98/influencer-coupon/server/models"
+)
+
+func main() {
+	// ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	// defer cancel()
+	cfg := config.Config{}
+	cfg.Load()
+
+	db := app.DbConnect(cfg)
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqlDB.Close()
+
+	// for i := range channels {
+	// 	utils.GetChannelIDAndNameFromHandle(ctx, cfg.YoutubeApiKey, &channels[i])
+	// }
+
+	// if err := seedChannels(db, channels); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	db.AutoMigrate(
+		&models.Channel{},
+		&models.Affiliate{},
+		&models.Video{},
+		&models.ChannelAffiliate{},
+		&models.ChannelAffiliateVideo{},
+	)
+	// log.Printf("seeded %d channels", len(channels))
+}
